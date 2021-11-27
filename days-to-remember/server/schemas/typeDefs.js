d7dc85b67b3c14dc2c12953ad8ce10a0f0928784
Jose Avalos <avaljose@hotmai.com>
2021-11-27 12:13:39 -0600

    working on schema

--- days-to-remember/server/schemas/typeDefs.js
+++ days-to-remember/server/schemas/typeDefs.js
@@ -1,5 +1,12 @@
-const { gql } = require('apollo-server-express');
+// const { gql } = require('apollo-server-express');
 
+// const typeDefs = gql`
+// type Users {
+//     _id: ID
+//     username: String
+//     email: String
+//     password: String
+    
+// }`
 
-
-module.exports = typeDefs;
+// module.exports = typeDefs;
