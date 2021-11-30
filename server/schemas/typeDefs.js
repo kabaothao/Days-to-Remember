07ac6b3e2e8bd0467dc4fa4d53c3c049452b5325
David Tierney <daveshouse44@hotmail.com>
2021-11-29 18:17:41 -0600

    added typeDefs and queries in resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,40 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    cards: [Card]!
+  }
 
+  type Card {
+    _id: ID
+    cardRecipient: String
+    cardText: String
+    cardSender: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me : User
+    user(username: String!): User
+    cards(username: String): [Card]
+    card(cardId: ID!): Card
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addCard(cardText: String!): Card
+    removeCard(cardId: ID!): Card
+  }
+`;
 
 module.exports = typeDefs;
