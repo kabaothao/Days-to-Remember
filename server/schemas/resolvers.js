9a8edd38c82abe202ce3783a8bf3c409dfb82b9d
David Tierney <daveshouse44@hotmail.com>
2021-11-30 13:34:18 -0600

    changed duplicate user seed, added mutations, added events to belong to user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -32,7 +32,57 @@ const resolvers = {
     },
   },
 
-//   Add mutations here
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    addEvent: async (parent, { title }, context) => {
+      if (context.user) {
+        const event = await Event.create({
+          title,
+          from: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { events: event._id } }
+        );
+
+        return event;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    removeEvent: async (parent, { eventId }, context) => {
+      if (context.user) {
+        const event = await Event.findOneAndDelete({
+          _id: eventId,
+          from: context.user.username,
+        });
+
+        await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { events: event._id } }
+        );
+
+        return event;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
 };
 
 module.exports = resolvers;
