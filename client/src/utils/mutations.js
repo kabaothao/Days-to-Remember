063ca733ab68ba7abbbc6b97b6763c702df92d0d
Kabao Thao <tkabao@gmail.com>
2021-11-30 20:18:27 -0600

    update queries and mutation files client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,2 +1,45 @@
 import { gql } from '@apollo/client';
 
+//use resolvers.js file as a guide to build mutations.
+
+//TODO:LOGIN_USER will execute the loginUser mutation set up using Apollo Server
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+//TODO:ADD_USER will execute the addUser mutation.
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+//TODO:ADD_EVENT will execute the ADD_EVENT mutation.
+
+export const ADD_EVENT = gql`
+  mutation addEvent($title: String!) {
+    addEvent(title: $title) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+//TODO:REMOVE_EVENT will execute the ADD_EVENT mutation.
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,29 @@
 import { gql } from '@apollo/client';
 
+//use typeDefs as a guide to build gql
+export const GET_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            password
+            events {
+                _id
+                title
+                name
+                phoneNum
+                date
+                from
+                createdAt
+            }
+            cards {
+                _id
+                cardRecipient
+                cardText
+                cardSender
+                createdAt
+            }
+        }
+    }
+`;
