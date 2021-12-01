a58e44d2b9e4155e70117b18ce8b6c0ce76115d1
Kabao Thao <tkabao@gmail.com>
2021-11-30 20:35:19 -0600

    update queries and mutation files client side again

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -28,18 +28,62 @@ export const ADD_USER = gql`
   }
 `;
 
+//1. use queries.js file as a guide to build ADD_EVENT  and REMOVE_EVENT
+//2. use typeDefs..js file as a guide to find type Mutation
 //TODO:ADD_EVENT will execute the ADD_EVENT mutation.
 
 export const ADD_EVENT = gql`
   mutation addEvent($title: String!) {
     addEvent(title: $title) {
-      token
-      user {
         _id
         username
-      }
+        email
+        password
+        events {
+            _id
+            title
+            name
+            phoneNum
+            date
+            from
+            createdAt
+        }
+        cards {
+            _id
+            cardRecipient
+            cardText
+            cardSender
+            createdAt
+        }
     }
   }
 `;
 
-//TODO:REMOVE_EVENT will execute the ADD_EVENT mutation.
+//TODO:REMOVE_EVENT will execute the REMOVE_EVENT mutation.
+
+export const REMOVE_EVENT = gql`
+  mutation removeEvent($eventId: ID!) {
+    addEvent(eventId: $eventId) {
+        _id
+        username
+        email
+        password
+        events {
+            _id
+            title
+            name
+            phoneNum
+            date
+            from
+            createdAt
+        }
+        cards {
+            _id
+            cardRecipient
+            cardText
+            cardSender
+            createdAt
+        }
+    }
+  }
+`;
