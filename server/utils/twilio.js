abae802b15e4c559ecbd2be8ab6461700e6b89f5
Alena Rehberger <alenarehberger92@gmail.com>
2021-12-01 19:17:58 -0600

    Commented out extra code

--- server/utils/twilio.js
+++ server/utils/twilio.js
@@ -1,6 +1,6 @@
-const client = require('twilio')(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_AUTH_TOKEN
-  );
+// const client = require('twilio')(
+//     process.env.TWILIO_ACCOUNT_SID,
+//     process.env.TWILIO_AUTH_TOKEN
+//   );
 
-  module.exports = client;
+//   module.exports = client;
