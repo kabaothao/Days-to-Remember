5ca8abcac2b1c1fff9e61a21ccb1c172614733e6
Alena Rehberger <alenarehberger92@gmail.com>
2021-12-04 00:19:35 -0600

    Style changes

--- client/src/pages/userForm/userForm.js
+++ client/src/pages/userForm/userForm.js
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
-import './userForm.css';
+// import React, { useState } from "react";
+// import { Form, Button } from "react-bootstrap";
+// import './userForm.css';
 
-const userForm = () => {
+// const userForm = () => {
 
-    return (
-      <div className="people-list-page">
-        <div className="people-list-wrapper">
-          <div className="people-list-container">
-            <h2 className="container-title">Birthdays Today</h2>
-            <li className="container-list">List of people here</li>
-            <li className="container-list">List of people here</li>
-            <li className="container-list">List of people here</li>
-          </div>
-        </div>
-      </div>
+//     return (
+//       <div className="people-list-page">
+//         <div className="people-list-wrapper">
+//           <div className="people-list-container">
+//             <h2 className="container-title">Birthdays Today</h2>
+//             <li className="container-list">List of people here</li>
+//             <li className="container-list">List of people here</li>
+//             <li className="container-list">List of people here</li>
+//           </div>
+//         </div>
+//       </div>
 
-    );
-}
+//     );
+// }
 
-export default userForm;
+// export default userForm;
